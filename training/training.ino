f5ee1fde37d3e6c36bd1aa48efb1ea73f25ab0a7
Jakub <j.kozdrowicz@apdeveloper.com.pl>
2019-11-26 16:41:09 +0100

    Training proceeds
    Podstawy C++

--- training/training.ino
+++ training/training.ino
@@ -1,9 +1,24 @@
+const int val5 = 6; //Stała globalna
+
 void setup() {
   // put your setup code here, to run once:
+Serial.begin(9600);
+Serial.println("Simple Calculation Using Functions");
 
+int num1 = 4;
+int num2;
+
+num2 = 23;
+
+Serial.println(do_a_calc(num1,num2));
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
 
+int do_a_calc(int val1,int val2) {
+  int val3;
+  val3 = val1 + val2 + val5;
+  return val3;
 }
