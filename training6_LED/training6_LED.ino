74d7d8c3f8c295f67b15f99a37192d60f9c58e8a
Jakub <j.kozdrowicz@apdeveloper.com.pl>
2019-11-27 19:03:56 +0100

    Skrócono za pomocą funkcji
    blink_led()

--- training6_LED/training6_LED.ino
+++ training6_LED/training6_LED.ino
@@ -7,16 +7,17 @@ void loop() {
   // Zwraca sygnał SOS.
   for (int counter = 1; counter < 5; counter++) {
     if (counter < 2 or counter > 3) { // Taka pętla, żeby sprawdzić czy działa "or"
-      digitalWrite(8, HIGH);
-      delay(500);
-      digitalWrite(8, LOW);
-      delay(500);
+      blink_led(8, 500);
     }
     else {
-      digitalWrite(8, HIGH);
-      delay(1000);
-      digitalWrite(8, LOW);
-      delay(1000);      
+      blink_led(8, 1000);    
     }
   }
+}
+
+void blink_led(int pin, int del) {
+      digitalWrite(pin, HIGH);
+      delay(del);
+      digitalWrite(pin, LOW);
+      delay(del);
 }
