1a7a6d16b5571f367f0b0ec07f4c5a9d9a282fbc
Jakub <j.kozdrowicz@apdeveloper.com.pl>
2019-11-27 18:57:07 +0100

    Dioda produkuje sygnał SOS
    Bo taką miałem fantazję. Przy okazji - "or" działa w C++ bezproblemowo.

--- training6_LED/training6_LED.ino
+++ training6_LED/training6_LED.ino
@@ -1,9 +1,22 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(
+  pinMode(8, OUTPUT); // Wyjście na pinie #8
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Zwraca sygnał SOS.
+  for (int counter = 1; counter < 5; counter++) {
+    if (counter < 2 or counter > 3) { // Taka pętla, żeby sprawdzić czy działa "or"
+      digitalWrite(8, HIGH);
+      delay(500);
+      digitalWrite(8, LOW);
+      delay(500);
+    }
+    else {
+      digitalWrite(8, HIGH);
+      delay(1000);
+      digitalWrite(8, LOW);
+      delay(1000);      
+    }
+  }
 }
