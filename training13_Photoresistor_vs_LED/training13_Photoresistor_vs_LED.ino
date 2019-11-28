ed53dc189c46dca6f75f3e009b3f4e71eeddf880
Jakub <j.kozdrowicz@apdeveloper.com.pl>
2019-11-28 18:15:38 +0100

    Mod zachowania FR
    Dioda wyłącza się przy dużej ilości światła ale nie głupieje, jak jest jej bardzo dużo (wartości ujemne).

--- training13_Photoresistor_vs_LED/training13_Photoresistor_vs_LED.ino
+++ training13_Photoresistor_vs_LED/training13_Photoresistor_vs_LED.ino
@@ -8,16 +8,18 @@ void loop() {
   // put your main code here, to run repeatedly:
   /// Analog read daje wartości od 0-1023
   int potValue = analogRead(A0);
-  Serial.print("Before conversion: ");
-  Serial.println(potValue);
-  delay(10);
 
   //Konwertuje wartości z jednej funkcji na drugą.
-  int mapppedValue = map(potValue, 0, 1023, 0, 255);
+  int mappedValue = map(potValue, 0, 1023, 0, 255);
+
+  int modValue = mappedValue - 10;
+  if (modValue < 0) {
+   modValue = 0;
+   }
     
   //Analog Write przyjmuje wartości od 0-255
-  analogWrite(6, mapppedValue);
+  analogWrite(6, modValue);
   Serial.print("After conversion: ");
-  Serial.println(mapppedValue);
-  delay(10);
+  Serial.println(modValue);
+  delay(100);
 }
