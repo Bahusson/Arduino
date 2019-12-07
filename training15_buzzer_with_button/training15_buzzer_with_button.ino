b836b5665b47b0769d0637e997b31cb809e1f611
Jakub <j.kozdrowicz@apdeveloper.com.pl>
2019-12-07 02:18:39 +0100

    Brzęczyk brzęczy na przyciśnięcie guzikiem.

---
+++ training15_buzzer_with_button/training15_buzzer_with_button.ino
@@ -0,0 +1,20 @@
+//const int button = 2;
+const int buzzer = 8;
+int buttonState = 0;
+
+void setup() {
+  pinMode(2,INPUT);
+  pinMode(buzzer, OUTPUT); // Ustaw głośnik jako wyjście.
+  Serial.begin(9600);
+}
+
+void loop() {
+   buttonState = digitalRead(button);
+   Serial.println(buttonState);
+   if (buttonState == HIGH) {
+    tone(buzzer, 100); // Wydaje dźwięk o czestotliwości 1KHz
+    delay(1000);
+    noTone(buzzer); // Wyłącz to wycie...
+    delay(1000);
+  }
+}
