54195db94386b34bd00da23719930b106d4410a8
Jakub <j.kozdrowicz@apdeveloper.com.pl>
2020-01-21 09:45:29 +0100

    Dodanie wyłącznika automatycznego

--- venta_damp_sensor/venta_damp_sensor.ino
+++ venta_damp_sensor/venta_damp_sensor.ino
@@ -1,15 +1,20 @@
 // Szkic dodaje inteligencji staremu niemieckiemu nawilżaczowi powietrza "Venta"
 // Napisany pod niepełnowartościwą (reklamacyjną) płytkę PRO Mini
 
+#include <Servo.h> //ściągnij bibliotekę obsługującą serwo
+
 #define G_LED  2  // LED zielony
 #define Y_LED  3  // LED Żółty
 #define R_LED  4  // LED Czerwony
 #define Buzzer 5  // Głośnik
+#define servoPin 6 // Serwo
 #define Waterlvl A1 // Czujnik poziomu wody
 
 int waterval = 0;  // wartość poziomu wody
 int waterbeep = 0; // dzięki temu na żółtym poziomie wody beeper daje znać tylko raz.
 unsigned int minute = 60000; // minuta w milisekundach
+Servo breaker; // Stwórz obiekt serwo - przerywacz.
+byte state = 1;
 
 void setup() {
   Serial.begin(9600);
@@ -20,7 +25,9 @@ void setup() {
   ledblinks(G_LED, 300, 1);
   ledblinks(Y_LED, 300, 1);
   ledblinks(R_LED, 300, 1);
+  //breaker_on();
   buzz_buzz(3);
+  //waterval = 1000;
 }
 
 void loop() {
@@ -32,29 +39,37 @@ void loop() {
     digitalWrite(Y_LED, LOW);
     digitalWrite(G_LED, LOW);
     buzz_buzz(2);
+    breaker_off();
+    state = 1;
     delay(minute/2);
-    } 
-  else if (waterval>100 && waterval<=650){ 
+    }  
+  else if (waterval>100 && waterval<=600){ 
     Serial.println("Water Level: Medium"); 
     digitalWrite(Y_LED, HIGH);
     digitalWrite(R_LED, LOW);
     digitalWrite(G_LED, LOW);
+    state = 1;
     if (waterbeep == 1){
       buzz_buzz(1);
       delay(minute);
       waterbeep = 0;
      }
-    } 
-  else if (waterval>650){ 
+    }   
+  else if (waterval>600){ 
     Serial.println("Water Level: High");
     digitalWrite(G_LED, HIGH);
     digitalWrite(R_LED, LOW);
     digitalWrite(Y_LED, LOW);
     waterbeep = 1;
+    if(state == 1){
+      breaker_on();
+      state = 0;
+     }
     }
-  delay(1000); 
+  delay(1000);
 }
 
+//FUNKCJE
 // Błyskacz ledów
 void ledblinks(int pin, int interval, int rep) { // Błyska Ledem "x" w interwale "y" razy "z".
     while (rep > 0) {
@@ -77,4 +92,21 @@ void buzz_buzz(byte rep){
   delay(b_del);
   rep -=1;
   }
+}
+
+
+//Głowica w pozycji "Włączonej"
+void breaker_on() {
+  breaker.attach(servoPin);
+  breaker.write(50);
+  delay(1000);
+  breaker.detach();
+}
+
+//Głowica w pozycji "Wyłączonej"
+void breaker_off(){
+  breaker.attach(servoPin);
+  breaker.write(70);
+  delay(1000);
+  breaker.detach();
 }
