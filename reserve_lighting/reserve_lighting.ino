1af65ff1558caa8772af50945f337a26b63a98b1
Jakub <j.kozdrowicz@apdeveloper.com.pl>
2020-02-11 11:39:36 +0100

    Dodano diody do przycisków i włącznik na fotorezystor
    Ponadto podłączono dwa najmniej używane pomieszczenia pod jeden włącznik.

--- reserve_lighting/reserve_lighting.ino
+++ reserve_lighting/reserve_lighting.ino
@@ -3,37 +3,29 @@
 
 // Zdefiniuj piny dla lampek i odpowiadających im
 // guzików/przełączników.
-#define workLight    2
-#define workButton   3
-#define corrLight    4
-#define corrButton   5
-#define bath1Light   6
-#define bath1Button  7
-#define gateLight    8
-#define gateButton   9
-#define bath2Light  10
-#define bath2Button 11
-#define kitchLight  12
-#define kitchButton 13
-#define smallLight  A0
-#define smallButton A1
-#define sleepLight  A2
-#define sleepButton A3
-#define machLight   A4
-#define machButton  A5
+#define workLight     2
+#define workButton    3
+#define corrLight     4
+#define corrButton    5
+#define bath1Light    6
+#define bath1Button   7
+#define gateLight     8  //Oświetlenie w maszynowni włącza się z guzika oświetlającego również wiatrołap. (hardware)
+#define gateButton    9
+#define bath2Light   10
+#define bath2Button  11
+#define kitchLight   12
+#define kitchButton  13
+#define smallLight   A0
+#define smallButton  A1
+#define sleepLight   A2
+#define sleepButton  A3
+#define buttonsLight A4 //Światła guzików przy włącznikach włączają się tylko po zmroku. (A6)
+// A5 pozostaje wolny.
+#define lightSensor  A6
+// Analog input A7 pozostaje wolny.
 
-
-// Zmienne trzymające stan poszczególnych przycisków.
+// Zmienna trzyma stan guzika.
 //int buttonState = 0;
-/*int workState = 0;
-int corrState = 0;
-int bath1State = 0;
-int gateState = 0;
-int bath2State = 0;
-int kitchState = 0;
-int smallState = 0;
-int sleepState = 0;
-int machState = 0;*/
 
 void setup() {
   // Przypisz piny związane z LEDami jako wyjścia.
@@ -46,14 +38,25 @@ void setup() {
   pinMode(smallLight, OUTPUT);
   pinMode(workLight, OUTPUT);
   pinMode(sleepLight, OUTPUT);
-  pinMode(machLight, OUTPUT);
+  pinMode(buttonsLight, OUTPUT);
   // Od razu po załączeniu oświetlenia awaryjnego
   // Załącz światło w kotłowni i na korytarzu.
-  digitalWrite(machLight, HIGH);
+  digitalWrite(gateLight, HIGH);
   digitalWrite(corrLight, HIGH);
 }
 
 void loop() {
+  int daytime = analogRead(lightSensor);
+  // Jak się robi ciemno, to zapala diody przy guzikach,
+  // żeby były łatwo widoczne i na odwrót.
+  if (daytime < 200) {
+    digitalWrite(buttonsLight, HIGH);
+    delay(5);
+  }
+  else {
+    digitalWrite(buttonsLight, LOW);
+    delay(5);
+  }
   // Umożliwia włączanie i wyłączanie wszystkich LEDów guzikami.
   switch_light(workLight, workButton);
   switch_light(corrLight, corrButton);
@@ -63,7 +66,6 @@ void loop() {
   switch_light(kitchLight, kitchButton);
   switch_light(smallLight, smallButton);
   switch_light(sleepLight, sleepButton);
-  switch_light(machLight, machButton);
   /* Pewnie da się to jakoś skrócić za pomocą 2 list
    *  i pętli FOR. Jak będzie czas, to spróbuję, ale najpierw
    *  hardware...*/
@@ -76,10 +78,10 @@ void switch_light(int defLED, int switch_button) {
   
   if (buttonState == HIGH && ledState == LOW){
     digitalWrite(defLED, HIGH);
-    delay(1000);
+    delay(500);
   }
   else if (buttonState == HIGH && ledState == HIGH){
     digitalWrite(defLED, LOW);
-    delay(1000);
+    delay(500);
   }
 }
