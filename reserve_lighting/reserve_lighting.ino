08d149d41f7d98f810b277dc27bd64ef27af8096
Jakub <j.kozdrowicz@apdeveloper.com.pl>
2020-02-15 13:33:01 +0100

    Dodano tolerancję czujnika światła

--- reserve_lighting/reserve_lighting.ino
+++ reserve_lighting/reserve_lighting.ino
@@ -21,14 +21,14 @@
 #define sleepButton  A3
 #define buttonsLight A4 //Światła guzików przy włącznikach włączają się tylko po zmroku. (A6)
 // A5 pozostaje wolny.
-#define lightSensor  A6
+#define lightSensor  A5 // A6 na Arduino PRO Mini
 // Analog input A7 pozostaje wolny.
-
-// Zmienna trzyma stan guzika.
-//int buttonState = 0;
-
+  
+unsigned int lightbuffer = 0;
+  
 void setup() {
   // Przypisz piny związane z LEDami jako wyjścia.
+  Serial.begin(9600);
   pinMode(workLight, OUTPUT);
   pinMode(corrLight, OUTPUT);
   pinMode(bath1Light, OUTPUT);
@@ -46,17 +46,12 @@ void setup() {
 }
 
 void loop() {
-  int daytime = analogRead(lightSensor);
-  // Jak się robi ciemno, to zapala diody przy guzikach,
-  // żeby były łatwo widoczne i na odwrót.
-  if (daytime < 200) {
-    digitalWrite(buttonsLight, HIGH);
-    delay(5);
+  // Sprawia, że światło na guzikach nie miga jak ktoś zasłoni czujnik na chwilę. Tolerancja 10s.
+  if (lightbuffer > 50) {
+    switch_visible();
+    lightbuffer = 0;
   }
-  else {
-    digitalWrite(buttonsLight, LOW);
-    delay(5);
-  }
+  Serial.println(lightbuffer);
   // Umożliwia włączanie i wyłączanie wszystkich LEDów guzikami.
   switch_light(workLight, workButton);
   switch_light(corrLight, corrButton);
@@ -69,19 +64,35 @@ void loop() {
   /* Pewnie da się to jakoś skrócić za pomocą 2 list
    *  i pętli FOR. Jak będzie czas, to spróbuję, ale najpierw
    *  hardware...*/
+  lightbuffer += 1;
 }
 
-//Przełącznik LEDów
+//Przełącznik LEDów (guziki)
 void switch_light(int defLED, int switch_button) {
   int ledState = digitalRead(defLED);
   int buttonState = digitalRead(switch_button);
   
   if (buttonState == HIGH && ledState == LOW){
     digitalWrite(defLED, HIGH);
-    delay(500);
+    delay(150);
   }
   else if (buttonState == HIGH && ledState == HIGH){
     digitalWrite(defLED, LOW);
-    delay(500);
+    delay(200);
+  }
+}
+
+//Przełącznik LEDów na guzikach (fotorezystor)
+void switch_visible() {
+  int daytime = analogRead(lightSensor);
+  // Jak się robi ciemno, to zapala diody przy guzikach,
+  // żeby były łatwo widoczne i na odwrót.
+  if (daytime < 200) {
+    digitalWrite(buttonsLight, HIGH);
+    delay(5);
+  }
+  else {
+    digitalWrite(buttonsLight, LOW);
+    delay(5);
   }
 }
